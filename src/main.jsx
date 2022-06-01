a4fffe1a96b84df2b6fa292cab20767d0895f4a0
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-01 03:37:35 +0200

    new site

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
-);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
--- src/pages/PageWelcome.jsx
+++
@@ -1,8 +0,0 @@
-export const PageWelcome = () => {
-	return (
-		<div className="page_welcome">
-			<h2>Welcome</h2>
-			<p>Welcome to this site.</p>
-		</div>
-	)
-}

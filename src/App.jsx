10eff71d3103a906af3567b28304b1f4e86f93de
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-01 03:53:32 +0200

    nouns set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import './App.scss'
+import './App.scss';
+import nouns from './data/germanNouns.json';
 
 function App() {
-  return (
-    <div className="App">
-		<h1>Info Site</h1>
-		<p>Welcome to this site.</p>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>German Article Practice</h1>
+			<div className="buttons">
+				<button>der</button>
+				<button>die</button>
+				<button>das</button>
+			</div>
+			<div className="nouns">
+				{nouns.map((noun, index) => {
+					return <div className="noun">{noun.article} {noun.singular}</div>;
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;

53eb4198528240bc6563393f1c3badf656dd176f
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-01 04:00:24 +0200

    buttons and state in

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import { useState } from 'react';
 import './App.scss';
 import nouns from './data/germanNouns.json';
 
 function App() {
+	const [article, setArticle] = useState('');
 	return (
 		<div className="App">
 			<h1>German Article Practice</h1>
 			<div className="buttons">
-				<button>der</button>
-				<button>die</button>
-				<button>das</button>
+				<button onClick={() => setArticle('der')}>der</button>
+				<button onClick={() => setArticle('die')}>die</button>
+				<button onClick={() => setArticle('das')}>das</button>
+				<button onClick={() => setArticle('')}>reset</button>
 			</div>
+			<div>{article}</div>
 			<div className="nouns">
 				{nouns.map((noun, index) => {
 					return <div className="noun">{noun.article} {noun.singular}</div>;

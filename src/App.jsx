41c0a4ad5eb9a852e7f888eb1eb83ae066b3090d
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-01 04:34:26 +0200

    firsr stage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,54 @@
 import { useState } from 'react';
 import './App.scss';
 import nouns from './data/germanNouns.json';
+import styled from 'styled-components';
+import * as qstr from './qstr';
+
+qstr.randomize(nouns);
+const Button = styled.button`
+	background-color: ${(props) =>
+		props.mode === 'selected' ? 'green' : '#eee'};
+`;
 
 function App() {
-	const [article, setArticle] = useState('');
+	const [choice, setChoice] = useState('hide');
 	return (
 		<div className="App">
-			<h1>German Article Practice</h1>
+			<h1>German choice Practice</h1>
 			<div className="buttons">
-				<button onClick={() => setArticle('der')}>der</button>
-				<button onClick={() => setArticle('die')}>die</button>
-				<button onClick={() => setArticle('das')}>das</button>
-				<button onClick={() => setArticle('')}>reset</button>
+				<Button
+					onClick={() => setChoice('der')}
+					mode={choice === 'der' ? 'selected' : 'unselected'}
+				>
+					der
+				</Button>
+				<Button
+					onClick={() => setChoice('die')}
+					mode={choice === 'die' ? 'selected' : 'unselected'}
+				>
+					die
+				</Button>
+				<Button
+					onClick={() => setChoice('das')}
+					mode={choice === 'das' ? 'selected' : 'unselected'}
+				>
+					das
+				</Button>
+				<Button
+					onClick={() => setChoice('')}
+					mode={choice === '' ? 'selected' : 'unselected'}
+				>
+					hide
+				</Button>
 			</div>
-			<div>{article}</div>
 			<div className="nouns">
 				{nouns.map((noun, index) => {
-					return <div className="noun">{noun.article} {noun.singular}</div>;
+					return (
+						<div className="noun">
+							{choice === noun.article && <>{noun.article}</>}{' '}
+							{noun.singular}
+						</div>
+					);
 				})}
 			</div>
 		</div>
